d6a09fe25a9149ce7d54185292316e2e814a9742
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2023-08-16 16:09:08 -0500

    proyecto, estructura e iniciando formulario de consulta

---
+++ app-api-bebidas/src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+<template>
+    <header>
+        <div class="mx-auto container px-5 py-16">
+            <div>
+
+            </div>
+            <form 
+                action=""
+                class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounde-lg shadow space-y-6"
+            >
+
+                <div class="space-y-4">
+                    <label class="block text-white uppercase font-extrabol text-lg" 
+                    for="ingrediente">Nombre o Ingredientes:</label>
+                    <input 
+                    id="ingrediente" 
+                    type="text" 
+                    class="p-3 w-full rounded-lg"
+                    placeholder="Nombre o Ingrediente">
+                </div>
+
+            </form>
+        </div>
+    </header>
+
+</template>
